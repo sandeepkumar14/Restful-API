10790eb7311f31f4faf2e203fad889b30c82059e
Sandeep Kumar <sandeep47momi@gmail.com>
2016-09-01 13:47:50 +0200

    Added config.js file, the mongodb database by using mongoose node package

---
+++ config.js
@@ -0,0 +1,6 @@
+ module.exports= {
+    "database": "mongodb://mysite:18062015@ds019866.mlab.com:19866/sandeepsite",
+    /* Environment Variable */
+    "port": (process.env.PORT || 5000),  
+    "secretKey": "238581sm",
+ }; 
